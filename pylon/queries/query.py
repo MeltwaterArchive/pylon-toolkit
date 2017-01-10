fb817a47a4ea4eec5e1566afbef1f77573cc31a5
Nathan Macnamara <nathan.macnamara@datasift.com>
2017-01-10 15:04:09 +0000

    Raise an exception if the results are redacted

--- pylon/queries/query.py
+++ pylon/queries/query.py
@@ -9,4 +9,9 @@ class Query(QueryBase):
 
     def df(self, level=-1):
         if 0 in self.results and self.results[0]['status'] == 'completed':
+            if self.results[0]['result']['analysis']['redacted']:
+                raise RedactedResults
             return Utils.pylon_response_to_dataframes(self.results[0])[level]
+
+class RedactedResults(Exception):
+    pass
