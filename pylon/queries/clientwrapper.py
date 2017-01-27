f713d06cfb82b818c3bc030d1ffe5271299d6c7e
Richard Caudle <richard.caudle@datasift.com>
2017-01-27 14:16:47 +0000

    Added clientwrapper aliases for mockclient

--- pylon/queries/clientwrapper.py
+++ pylon/queries/clientwrapper.py
@@ -5,6 +5,8 @@ class ClientWrapper(object):
         # Setup aliases
         if self.client_type(client) == 'datasift.client.Client':
             self.aliases_ds_client()
+        else:
+            self.aliases_default()
 
     def client_type(self, o):
         return o.__module__ + "." + o.__class__.__name__
@@ -12,3 +14,7 @@ class ClientWrapper(object):
     def aliases_ds_client(self):
         self.create_task = self.client.pylon.task.create
         self.get_task = self.client.pylon.task.get
+
+    def aliases_default(self):
+        self.create_task = self.client.create_task
+        self.get_task = self.client.get_task
